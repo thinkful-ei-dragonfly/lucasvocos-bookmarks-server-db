3d98b54e4878b5fb3214cd8dbdb993164611b232
Lucas Vocos <lucas.vocos@me.com>
2019-06-22 09:56:27 -0400

    test logging for heroku

--- src/validate-bearer-token.js
+++ src/validate-bearer-token.js
@@ -4,7 +4,7 @@ const logger = require('./logger')
 function validateBearerToken(req, res, next) {
   const authToken = req.get('Authorization')
   logger.error(`Unauthorized request to path: ${req.path}`)
-  debugger;
+  console.log(authToken)
   if (!authToken || authToken.split(' ')[1] !== API_TOKEN) {
     return res.status(401).json({ error: 'Unauthorized request' })
   }
